aac6cd9ed0724b278650d4f9f28e104401036156
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-01-30 10:22:28 +0530

    addded project info

--- extractor.py
+++ extractor.py
@@ -1,4 +1,4 @@
-import json
+import colorama
 
 
 class Extractor:
@@ -9,25 +9,53 @@ class Extractor:
 
         intro = data['included'][-1]
         included = data['included']
-        print(intro)
-        print(included[0])
+        # print(intro)
+        # print(data)
+        # print(included)
 
-        firstName = intro['firstName']
-        lastName = intro['lastName']
-        occupation = intro['occupation']
-
+        firstName = ''
+        lastName = ''
+        occupation = ''
+        summary = ''
+        courses = []
         skills = []
         experience = []
+        organization_name = []
+        organization_discription = []
+        project_name = []
+        project_discription = []
+        project_url = []
 
         for index, i in enumerate(included):
-            if 'name' in i.keys():
-                print(i)
-                print(index)
+            keys = i.keys()
+            if 'summary' in keys:
+                firstName = i['firstName']
+                lastName = i['lastName']
+                occupation = i['headline']
+                summary = i['summary']
+            elif 'name' in keys:
+                # print(i)
+                # print(index)
                 if (len(i) == 9):
-                    print(i['name'] + 'company')
+                    # print(i)
+                    # print(i['name'] + 'company**************************')
                     experience.append(i['name'])
                 if (len(i) == 4):
                     skills.append(i['name'])
+                if (len(i) == 6):
+                    courses.append(i['name'])
+                if (len(i) == 8):
+                    organization_name.append(i['name'])
+                    organization_discription.append(i['description'])
+            elif 'members' in keys:
+                # print(i)
+                project_name.append(i['title'])
+                project_discription.append(i['description'])
+                if 'url' in keys:
+                    project_url.append(i['url'])
+                else:
+                    project_url.append('')
+
 
         skillsList = ''
         for i in skills:
@@ -37,8 +65,25 @@ class Extractor:
         for i in experience:
             experienceList += (i + ', ');
 
-        print('\n' + 'Name:       ' + firstName + ' ' + lastName + '\n'
-                'Occupation: ' + occupation + '\n'
-                'Skills:     ' + skillsList[:-2] + '\n'
-                'Experience: ' + experienceList[:-2])
+        courseList = ''
+        for i in courses:
+            courseList += (i + ', ');
+
+        organizationList = '\n'
+        for i in range(len(organization_name)):
+            organizationList += '   '+(organization_name[i] + ' :- \n'+'        '+organization_discription[i]+'\n');
+
+        projectList = '\n'
+        for i in range(len(project_name)):
+            projectList += '   ' +colorama.Fore.GREEN+ (project_name[i] + ' :- \n' + '        ' +colorama.Fore.WHITE+ project_discription[i] + '\n'+ project_url[i] + '\n'+'\n');
+
+        print('\n' + 'Name:       ' + firstName + ' ' + lastName + '\n---------------------------------------------------------------------------\n'
+                'Occupation: ' + occupation + '\n---------------------------------------------------------------------------\n'
+                'Summary: ' + summary + '\n---------------------------------------------------------------------------\n'
+                'Skills:     ' + skillsList[:-2] + '\n---------------------------------------------------------------------------\n'
+                'Experience: ' + experienceList[:-2]+ '\n---------------------------------------------------------------------------\n'
+                'Courses: ' + courseList[:-2]+ '\n---------------------------------------------------------------------------\n'
+                'Organizations: ' + organizationList+ '\n---------------------------------------------------------------------------\n'
+                'Projects: ' + projectList
+              )
 
